0ae2ea3f6eae1a1030700b0092a250519eacc400
ikhwanyoza <ikhwanyoza@gmail.com>
2020-10-19 14:07:19 +0700

    adding python code

---
+++ condition.py
@@ -0,0 +1,5 @@
+a = int(input("Masukan Nilai Akhir : "))
+if a >= 80:
+    print("selamat anda lulus")
+else:
+    print("anda harus mengulang")
---
+++ main.py
@@ -0,0 +1,11 @@
+#ini adalah baris komentar
+print("apa kabar") #ini adalah baris print
+#print("Hello World")
+a = input()
+print("hello "+a)
+
+"""
+ini komentar baris 1
+ini komentar baris 2
+dst...
+"""
